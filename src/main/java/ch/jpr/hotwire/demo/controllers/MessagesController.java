bb875654be792edb6dd6b73d42ea52f9e6a542cd
Joachim Praetorius <joachim.praetorius@innoq.com>
2021-01-08 16:54:55 +0100

    add lazy loaded turbo-frames

--- src/main/java/ch/jpr/hotwire/demo/controllers/MessagesController.java
+++ src/main/java/ch/jpr/hotwire/demo/controllers/MessagesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,6 +20,13 @@ public class MessagesController {
 
     public List<String> getComments() {
         return this.comments;
+    }
+
+    @GetMapping(path = "/messages/comments")
+    public String comments(Model model) throws InterruptedException {
+        Thread.sleep(1000);
+        model.addAttribute("comments", this.comments);
+        return "comments";
     }
 
     @PostMapping(path = "/messages/comments")
